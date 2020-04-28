908b75337da8e0cf6052457f28cce69b78844773
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-04-28 21:33:24 +0100

    Added QH namespace

--- Source/QuantumHierarchy.cs
+++ Source/QuantumHierarchy.cs
@@ -5,57 +5,61 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class QuantumHierarchy : EditorWindow
+namespace QFSW.QH
 {
-    private Vector2 _scroll;
-    private readonly List<GameObject> _rootGameObjectBuffer = new List<GameObject>();
-
-    [MenuItem("Window/Quantum Hierarchy")]
-    public static void Create()
+    public class QuantumHierarchy : EditorWindow
     {
-        GetWindow<QuantumHierarchy>(false, "Quantum Hierarchy");
-    }
+        private Vector2 _scroll;
+        private readonly List<GameObject> _rootGameObjectBuffer = new List<GameObject>();
 
-    void OnGUI()
-    {
-        _scroll = EditorGUILayout.BeginScrollView(_scroll);
-        int sceneCount = SceneManager.sceneCount;
-        for (int i = 0; i < sceneCount; i++)
+        [MenuItem("Window/Quantum Hierarchy")]
+        public static void Create()
         {
-            Scene scene = SceneManager.GetSceneAt(i);
-
-            if (scene.isLoaded)
-            {
-                EditorGUILayout.LabelField(scene.name, EditorStyles.boldLabel);
-
-                _rootGameObjectBuffer.Clear();
-                scene.GetRootGameObjects(_rootGameObjectBuffer);
-
-                foreach (GameObject root in _rootGameObjectBuffer)
-                {
-                    EditorGUILayout.BeginHorizontal();
-
-                    EditorGUILayout.ObjectField(root, typeof(GameObject), true);
-
-                    HideFlags flags = (HideFlags)EditorGUILayout.EnumFlagsField(root.hideFlags);
-                    if (flags != root.hideFlags)
-                    {
-                        root.hideFlags = flags;
-                        EditorSceneManager.MarkSceneDirty(scene);
-                    }
-
-                    if (QGUILayout.ButtonAuto(new GUIContent("Delete"), EditorStyles.miniButton))
-                    {
-                        DestroyImmediate(root);
-                        EditorSceneManager.MarkSceneDirty(scene);
-                    }
-
-                    EditorGUILayout.EndHorizontal();
-                }
-
-                EditorGUILayout.Space();
-            }
+            GetWindow<QuantumHierarchy>(false, "Quantum Hierarchy");
         }
-        EditorGUILayout.EndScrollView();
+
+        void OnGUI()
+        {
+            _scroll = EditorGUILayout.BeginScrollView(_scroll);
+            int sceneCount = SceneManager.sceneCount;
+            for (int i = 0; i < sceneCount; i++)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+
+                if (scene.isLoaded)
+                {
+                    EditorGUILayout.LabelField(scene.name, EditorStyles.boldLabel);
+
+                    _rootGameObjectBuffer.Clear();
+                    scene.GetRootGameObjects(_rootGameObjectBuffer);
+
+                    foreach (GameObject root in _rootGameObjectBuffer)
+                    {
+                        EditorGUILayout.BeginHorizontal();
+
+                        EditorGUILayout.ObjectField(root, typeof(GameObject), true);
+
+                        HideFlags flags = (HideFlags) EditorGUILayout.EnumFlagsField(root.hideFlags);
+                        if (flags != root.hideFlags)
+                        {
+                            root.hideFlags = flags;
+                            EditorSceneManager.MarkSceneDirty(scene);
+                        }
+
+                        if (QGUILayout.ButtonAuto(new GUIContent("Delete"), EditorStyles.miniButton))
+                        {
+                            DestroyImmediate(root);
+                            EditorSceneManager.MarkSceneDirty(scene);
+                        }
+
+                        EditorGUILayout.EndHorizontal();
+                    }
+
+                    EditorGUILayout.Space();
+                }
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
     }
 }
